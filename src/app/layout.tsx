1df7dcc17343c81b8fa6d222e268cdd9e0312e0c
Yureks Solutions <yureksolutions@gmail.com>
2026-05-25 17:17:02 +0530

    Add multiple favicon rel types for better browser support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,6 +88,7 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <head>
         <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
